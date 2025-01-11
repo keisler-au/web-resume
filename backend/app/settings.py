80681cb6c5f8baed70bc8f824c945e5ad9a7b058
Josh K <keisler@live.com>
2025-01-11 19:45:18 +0100

    chore Added logging

--- backend/app/settings.py
+++ backend/app/settings.py
@@ -61,8 +61,11 @@ CORS_ALLOWED_ORIGINS = ["http://localhost:3000"]
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
 if ENV == "Production":
-    CORS_ALLOWED_ORIGINS.append(f"https://${os.getenv("HOST_DOMAIN")}")
-
+    CORS_ALLOWED_ORIGINS = [
+        "https://joshkeisler.com",
+        "https://www.joshkeisler.com",
+        "https://45.77.142.136",
+    ]
     EMAIL_BACKEND = "anymail.backends.mailgun.EmailBackend"
     ANYMAIL = {
         "MAILGUN_API_KEY": os.getenv("MAILGUN_API_KEY"),
